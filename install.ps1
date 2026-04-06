f13e060e3a0b29489f36aa025b44bd3b3d56a58f
Adam Haziq <adamproml123@gmail.com>
2026-04-06 23:36:40 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -23,7 +23,7 @@ Install-App "WinRAR" "RARLab.WinRAR"
 Install-App "VLC Player" "VideoLAN.VLC"
 
 #Office 2013 installation
-$officeUrl = ""
+$officeUrl = "https://github.com/adamproml123-jpg/basicsoft/releases/tag/v1.0/Office2013Setup.exe"
 $officeFile = "$env:TEMP\Office2013Setup.exe"
 
 Write-Host "[DOWNLOADING] Office 2013..." -ForegroundColor Yellow
