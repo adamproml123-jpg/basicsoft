d406ceb838fadf870e30e981163a5aaca08ba89b
Adam Haziq <adamproml123@gmail.com>
2026-04-06 23:35:23 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -22,15 +22,17 @@ Install-App "Google Chrome" "Google.Chrome"
 Install-App "WinRAR" "RARLab.WinRAR"
 Install-App "VLC Player" "VideoLAN.VLC"
 
-# Office (manual installer)
-Write-Host "[INFO] Installing Office 2013..." -ForegroundColor Yellow
+#Office 2013 installation
+$officeUrl = ""
+$officeFile = "$env:TEMP\Office2013Setup.exe"
 
-if (Test-Path ".\Office2013Setup.exe") {
-    Start-Process ".\Office2013Setup.exe" -ArgumentList "/quiet" -Wait
-    Write-Host "[DONE] Office 2013 Installed" -ForegroundColor Green
-} else {
-    Write-Host "[SKIPPED] Office installer not found in folder" -ForegroundColor Red
-}
+Write-Host "[DOWNLOADING] Office 2013..." -ForegroundColor Yellow
+Invoke-WebRequest $officeUrl -OutFile $officeFile
+
+Write-Host "[INSTALLING] Office 2013..." -ForegroundColor Yellow
+Start-Process $officeFile -ArgumentList "/quiet" -Wait
+
+Write-Host "[DONE] Office 2013 Installed" -ForegroundColor Green
 
 Write-Host ""
 Write-Host "ALL TASKS COMPLETED!" -ForegroundColor Cyan
