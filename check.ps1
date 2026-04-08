82463ed836c55e93cf91bab18e33a294e8a35cbd
Adam Haziq <adamproml123@gmail.com>
2026-04-09 00:54:19 +0800

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -23,40 +23,22 @@ function Test-Installed {
 
 Write-Host "Checking installed software..." -ForegroundColor Cyan
 
-$vlc     = Test-Installed "VLC"
 $winrar  = Test-Installed "WinRAR"
-$chrome  = Test-Installed "Google Chrome"
-$office  = Test-Installed "Office 2013"
-$vcredist= Test-Installed "Visual C++"
 
-# Show results
-Write-Host "VLC: $vlc"
-Write-Host "WinRAR: $winrar"
-Write-Host "Chrome: $chrome"
-Write-Host "Office 2013: $office"
-Write-Host "Visual C++: $vcredist"
 
 # ================================
 # CHECK ALL
 # ================================
 
-if ($vlc -and $winrar -and $chrome -and $office -and $vcredist) {
+if ($winrar) {
 
-    Write-Host "`nAll software is installed. Running next commands..." -ForegroundColor Green
-
-    # 👉 PUT YOUR NEXT COMMANDS HERE
-    Write-Host "Running next step..."
-    
-    # Example:
-    # Start-Process "notepad.exe"
+    Write-Host "Work"
 
 }
 else {
     Write-Host "`nSome software is missing!" -ForegroundColor Red
 
-    if (-not $vlc)      { Write-Host "- VLC missing" }
+    
     if (-not $winrar)   { Write-Host "- WinRAR missing" }
-    if (-not $chrome)   { Write-Host "- Chrome missing" }
-    if (-not $office)   { Write-Host "- Office 2013 missing" }
-    if (-not $vcredist) { Write-Host "- Visual C++ missing" }
+    
 }
