e23ff6776f532dcc221bfe5a6df672ba3530643d
Adam Haziq <adamproml123@gmail.com>
2026-04-08 23:06:59 +0800

    Update vlctest.ps1

--- vlctest.ps1
+++ vlctest.ps1
@@ -2,12 +2,13 @@ Show-Step "Installing VLC Player..."
 $VLCPath = "$Downloads\vlc.exe"
 
 try {
-    Start-Process "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe"
+    Invoke-WebRequest -Uri "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe" `
+            -OutFile $VLCPath `
+            -Headers @{ "User-Agent" = "Mozilla/5.0" }
+    
 }
 catch {
-    Invoke-WebRequest -Uri "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe" `
-        -OutFile $VLCPath `
-        -Headers @{ "User-Agent" = "Mozilla/5.0" }
+    Start-Process "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe"
 }
 
 Start-Process $VLCPath
