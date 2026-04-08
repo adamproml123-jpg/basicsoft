cb30168478ce3eeaecc7faacf30df925b4529d4e
Adam Haziq <adamproml123@gmail.com>
2026-04-08 23:18:27 +0800

    Update installfast.ps1

--- installfast.ps1
+++ installfast.ps1
@@ -28,15 +28,15 @@ Show-Step "Installing VLC Player..."
 $VLCPath = "$Downloads\vlc.exe"
 
 try {
-    Start-Process "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe"
+    Start-BitsTransfer -Source "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe" -Destination $VLCPath
 }
 catch {
     Invoke-WebRequest -Uri "https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe" `
         -OutFile $VLCPath `
         -Headers @{ "User-Agent" = "Mozilla/5.0" }
 }
-
-#Start-Process $VLCPath
+Get-Process vlc -ErrorAction SilentlyContinue | Stop-Process -Force
+Start-Process $VLCPath -Wait
 
 Show-Done "VLC Player Installed"
 # =========================
